6ee50cbc0f8bf07d731efc24eb5649c00a5e5039
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2025-02-06 09:06:55 +0530

    Update modifiers.java

--- src/Oops/Modifiers.java
+++ src/Oops/Modifiers.java
@@ -1,9 +1,31 @@
 package Oops;
 
+class example{
+    public int a = 10;
+    protected int b = 20;
+}
+
+
+
 public class Modifiers {
     public int a = 10;
+    protected int b = 20;
+    int c = 30;
+    private int d = 40;
     public static void main(String[] args) {
         Modifiers obj = new Modifiers();
         System.out.println(obj.a);
+        System.out.println(obj.b);
+        System.out.println(obj.c);
+        System.out.println(obj.d);
+        System.out.println("\n");
+
+
+        example ex = new example();
+        System.out.println(ex.a);
+        System.out.println(ex.b);
     }
+
+
+
 }
