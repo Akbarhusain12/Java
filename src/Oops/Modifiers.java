e7fa593c7535c6b9cec083a7e545921d8df85dad
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2025-02-06 08:52:26 +0530

    add new file

---
+++ src/Oops/Modifiers.java
@@ -0,0 +1,9 @@
+package Oops;
+
+public class Modifiers {
+    public int a = 10;
+    public static void main(String[] args) {
+        Modifiers obj = new Modifiers();
+        System.out.println(obj.a);
+    }
+}
