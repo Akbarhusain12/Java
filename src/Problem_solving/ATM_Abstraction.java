6a3a7a02f724116d55f4d60e82856f5d537a89d7
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2025-03-13 17:02:33 +0530

    add new method

--- src/Problem_solving/ATM_Abstraction.java
+++ src/Problem_solving/ATM_Abstraction.java
@@ -2,43 +2,43 @@ package Problem_solving;
 
 import java.util.Scanner;
 
-abstract class ATM{
+abstract class ATM {
     double balance;
-    ATM(double balance){
+
+    ATM(double balance) {
         this.balance = balance;
     }
+
     abstract void withdraw(double balance);
     abstract void deposit(double balance);
-    abstract void checkBal();  // This is the method that needs to be implemented
+    abstract void checkBal();
 }
 
-class ICICI extends ATM{
-    ICICI(double balance){
+class ICICI extends ATM {
+    ICICI(double balance) {
         super(balance);
     }
 
-    void withdraw(double amount){
-        if (amount > 0 && amount <= balance){
+    void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
             balance -= amount;
-            System.out.println("Withdraw Successfully");
-        }
-        else{
-            System.out.println("Balance is Not Available");
+            System.out.println("Withdrawn Successfully");
+        } else {
+            System.out.println("Insufficient Balance");
         }
     }
 
-    void deposit(double amount){
-        if(amount > 0){
+    void deposit(double amount) {
+        if (amount > 0) {
             balance += amount;
-            System.out.println("Amount is Deposited");
-        }
-        else {
+            System.out.println("Amount Deposited Successfully");
+        } else {
             System.out.println("Invalid Amount");
         }
     }
 
-    void checkBal(){  // Changed from chekbal() to checkBal()
-        System.out.println("Available balance: " + balance);
+    void checkBal() {
+        System.out.println("Available Balance: " + balance);
     }
 }
 
@@ -47,37 +47,31 @@ public class ATM_Abstraction {
         Scanner sc = new Scanner(System.in);
         ICICI obj = new ICICI(1000);
 
-        while (true){
-            System.out.println("---ATM MENU ---");
-            System.out.println("1. Withdraw Money");
-            System.out.println("2. Deposit Money");
-            System.out.println("3. Check Money");
-            System.out.println("4. Exit");
-            System.out.println("---------");
+        while (true) {
+            System.out.print("""
+            ---ATM MENU---
+            1. Withdraw Money
+            2. Deposit Money
+            3. Check Money
+            4. Exit
+            Enter Your Choice: """);
 
-            System.out.print("Enter Your Choice: ");
-            int choice = sc.nextInt();
-
-            switch (choice){
-                case 1:
+            switch (sc.nextInt()) {
+                case 1 -> {
                     System.out.print("Enter amount to withdraw: ");
-                    double with_amt = sc.nextDouble();
-                    obj.withdraw(with_amt);
-                    break;
-                case 2:
+                    obj.withdraw(sc.nextDouble());
+                }
+                case 2 -> {
                     System.out.print("Enter amount to deposit: ");
-                    double depo_amt = sc.nextDouble();
-                    obj.deposit(depo_amt);
-                    break;
-                case 3:
-                    obj.checkBal();
-                    break;
-                case 4:
+                    obj.deposit(sc.nextDouble());
+                }
+                case 3 -> obj.checkBal();
+                case 4 -> {
                     System.out.println("Thank you for using ICICI ATM");
                     sc.close();
-                    return;
-                default:
-                    System.out.println("Invalid Option, Please select correct option.");
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid Option, Please select a correct option.");
             }
         }
     }
