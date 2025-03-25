187a33df35b866d1c8d3fd105e09966ea02e78b1
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2025-03-25 14:23:48 +0530

    Updated code

--- src/Oops/Excep_handling.java
+++ src/Oops/Excep_handling.java
@@ -8,9 +8,20 @@ public class Excep_handling {
 
             int div = a / b;
             System.out.println("Division is: " + div);
+
+            int[] numbers = {10, 20, 30, 40, 50};
+
+            System.out.println("Array elements:");
+            for (int num : numbers) {
+                System.out.println(num);
+            }
+
         } catch (ArithmeticException e) {
             throw new RuntimeException("An error occurred: Division by zero", e);
-        } finally {
+        }catch(ArrayIndexOutOfBoundsException A){
+            System.out.println("Array index is out of bounds.");
+        }
+        finally {
             System.out.println("Finally block executed.");
         }
     }
