db9084fa1d3105203e1d5b00caa926c62217a9e6
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2025-04-15 14:29:49 +0530

    add new file

---
+++ src/File_Handling/Threads.java
@@ -0,0 +1,21 @@
+package File_Handling;
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread is Starting..." + i);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread is not Starting.. " + e);
+            }
+        }
+    }
+}
+
+public class Threads {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+    }
+}
